3f82afed7703cf28069d2e823c65229de8aaa1f8
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2025-08-14 19:27:00 -0300

    Spring security checklist, not tested

---
+++ src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscommerce.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable());
+		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+		return http.build();
+	}
+	
+	SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
+		http.securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable());
+		return http.build();
+	}
+}
